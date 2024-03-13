39591a8503547ba3630468d180d40c46602adadd
faxriddin <faxu@gmail.com>
2024-03-13 11:29:39 +0500

    f

---
+++ script.js
@@ -0,0 +1,16 @@
+var yow = +prompt('yowingizni kiriting')
+if (yow<=18){
+  alert('siz oqiwiz kk')
+}else if(yow<=60){
+  alert('siz ishlawingiz kk')
+}else if(yow<=90){
+  alert('siz damoliwiz kk')
+}else if(yow<=100){
+  alert('siz tirik bolsangiz damizi olin vaqt ganimat')
+}
+else if(yow<=4){
+  alert('siz yowsiz boxcaga borin')
+}
+else{
+alert('nmadur xato tekwir')
+}
